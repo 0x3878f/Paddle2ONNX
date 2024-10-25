94d48563577c62cb7dfdd4210944e23833918305
qzylalala <304228244@qq.com>
2024-10-25 16:00:13 +0800

    [OSPP][PIR] suport numel

--- paddle2onnx/mapper/activation/activation.h
+++ paddle2onnx/mapper/activation/activation.h
@@ -261,6 +261,13 @@ class SizeMapper : public Mapper {
              int64_t block_id,
              int64_t op_id)
       : Mapper(p, helper, block_id, op_id) {}
+  SizeMapper(const PaddlePirParser& p,
+             OnnxHelper* helper,
+             int64_t op_id,
+             bool c)
+      : Mapper(p, helper, op_id, c) {
+    in_pir_mode = true;
+  }
   void Opset7() override;
 };
 
