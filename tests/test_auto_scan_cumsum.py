f57cb291e2982485abbc4380f9ec02accfcf6387
qzylalala <304228244@qq.com>
2024-11-08 17:36:55 +0800

    [PIR] support cumsum

--- tests/test_auto_scan_cumsum.py
+++ tests/test_auto_scan_cumsum.py
@@ -16,6 +16,7 @@ from auto_scan_test import OPConvertAutoScanTest, BaseNet
 import hypothesis.strategies as st
 import unittest
 import paddle
+from onnxbase import _test_only_pir
 
 
 class Net(BaseNet):
@@ -83,7 +84,7 @@ class TestCumsumConvert(OPConvertAutoScanTest):
 
         return (config, models)
 
-    # @_test_only_pir
+    @_test_only_pir
     def test(self):
         self.run_and_statis(max_examples=30)
 
