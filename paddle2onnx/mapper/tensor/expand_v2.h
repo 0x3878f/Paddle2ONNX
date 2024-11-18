f9e0a726ef093add95eeefc267e7465a0d0b266b
qzylalala <304228244@qq.com>
2024-11-18 23:03:54 +0800

    support expand

--- paddle2onnx/mapper/tensor/expand_v2.h
+++ paddle2onnx/mapper/tensor/expand_v2.h
@@ -24,7 +24,9 @@ class ExpandV2Mapper : public Mapper {
       : Mapper(p, helper, block_id, op_id) {}
   ExpandV2Mapper(const PaddlePirParser& p, OnnxHelper* helper,
                  int64_t op_id, bool c)
-      : Mapper(p, helper, op_id, c) { in_pir_mode = true; }
+      : Mapper(p, helper, op_id, c) { 
+    in_pir_mode = true; 
+  }
   int32_t GetMinOpsetVersion(bool verbose) override {
     Logger(verbose, 8) << RequireOpset(8) << std::endl;
     return 8;
