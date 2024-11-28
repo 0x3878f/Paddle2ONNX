2d11e88ab4708c1aefafaadde35df62adcece6a7
Zhenghai Zhang <65210872+ccsuzzh@users.noreply.github.com>
2024-11-28 20:08:19 +0800

    cherry-picking commit 73e9f6c: Add MacRelease (#1445)
    * test build on macos-latest
    
    * fix bug
    
    * test entrypoint.sh
    
    * fix bug
    
    * test build on macos-latest
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * test release_mac
    
    * fix bug
    
    * test release_mac
    
    * fix bug
    
    * skip test_empty
    
    * test release mac
    
    * finish release_mac test

--- .github/workflows/scripts/download_protobuf.sh
+++ .github/workflows/scripts/download_protobuf.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Detect the operating system
 OS=$(uname -s)
 ARCH=$(uname -m)
@@ -30,7 +44,7 @@ else
    echo "The system only supports (Linux and Darwin), but the current system is $OS."
 fi
 
-wget -q $protobuf_url
+wget $protobuf_url
 protobuf_save_dir="$PWD/installed_protobuf"
 mkdir -p $protobuf_save_dir
 tar -zxf $protobuf_tgz_name -C $protobuf_save_dir
