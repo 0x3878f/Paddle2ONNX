65b78510f0ac83d8e5e5bba707ddf27904c7d54b
risemeup1 <515586620@qq.com>
2025-02-12 17:24:02 +0000

    fix

--- tests/test_fft_r2c.py
+++ tests/test_fft_r2c.py
@@ -44,9 +44,9 @@ def test_fftr2c_17():
     op = Net()
     op.eval()
     # net, name, ver_list, delta=1e-6, rtol=1e-5
-    obj = APIOnnx(op, "fft_r2c", [17], delta=1e-3)
+    obj = APIOnnx(op, "fft_r2c", [17])
     obj.set_input_data(
         "input_data",
-        paddle.to_tensor(randtool("float", -1, 1, [2, 192, 32]).astype("float32")),
+        paddle.to_tensor(randtool("float", -1, 1, [3, 10, 10]).astype("float32")),
     )
     obj.run()
